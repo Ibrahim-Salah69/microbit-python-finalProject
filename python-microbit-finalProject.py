827bcab0799cc4d41df0c2fd1693b39e8f00b72d
Ibrahim-Salah69 <107865794+Ibrahim-Salah69@users.noreply.github.com>
2022-07-06 16:45:54 +0300

    some code edits

--- python-microbit-finalProject.py
+++ python-microbit-finalProject.py
@@ -6,23 +6,30 @@ clear_oled()
 
 pulseList = []
 
-while True:
+while True: 
     if pin2.read_digital() == 1:
+        add_text(0, 1, "machine ")
+        add_text(0, 2, "learning on ")
         #pulseList.clear()
         if pin1.read_digital() == 1:
             pin16.write_analog(1000)
             pulseList.append(1000)
             sleep(100)
-
-        else:=
+        else:
             pin16.write_analog(0)
             pulseList.append(0)
             sleep(100)
+    
+    else:
+        pin16.write_analog(0)
+        add_text(0, 1, "machine ")
+        add_text(0, 2, "learning off")
+        add_text(0, 3, ".......")
 
     add_text(0, 0, str(pulseList))
 
     if pin0.read_digital() == 1:
+        add_text(0, 3, "playing")
         for i in pulseList:
             pin16.write_analog(i)
             sleep(100)
-
